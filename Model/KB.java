b9dd0096b6cb25b024e50609c7d5d26ba70dd8a3
Yi-Chen-Lin2019 <58112802+Yi-Chen-Lin2019@users.noreply.github.com>
2019-12-09 15:19:44 +0100

    KB upload

---
+++ Model/KB.java
@@ -0,0 +1,32 @@
+package Model;
+import java.util.*;
+
+public class KB extends ItemDescriptor
+{
+    private double price;
+    private ArrayList<ItemDescriptor> products;
+
+    public KB(String barcode, String name, double price)
+    {
+        super(barcode, name);
+        this.price = price;
+        products = new ArrayList<>();
+    }
+    public double getSellingPrice()
+    {
+        return price;
+    }
+    public ArrayList<ItemDescriptor> getProducts()
+    {
+        return products;
+    }
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+    public void addProduct(ItemDescriptor product)
+    {
+        products.add(product);
+    }
+    
+}
