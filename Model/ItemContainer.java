7339ba179394f81bb28fd8af59fd5a9efac28bc1
arsendr <51822852+arsendr@users.noreply.github.com>
2019-12-10 10:20:57 +0100

    Update ItemContainer.java

--- Model/ItemContainer.java
+++ Model/ItemContainer.java
@@ -31,4 +31,60 @@ public class ItemContainer
     {
         return items;
     }
+    
+    public void saveItem()
+    {
+        String outString;
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("Products.txt"));
+            // Write these lines to the file.
+            // ... We call newLine to insert a newline character.
+            try {
+                outString = String.format("%d",items.size());
+                writer.write(outString);
+                writer.newLine();
+            }
+            catch (Exception e) {
+
+            }
+            for (ItemDescriptor item : items) {
+
+                try {
+                    writer.write(item.getBarcode());
+                    writer.newLine();
+                    writer.write(item.getName());
+                    writer.newLine();
+                    outString = String.format("%d",item.getCategoryID());
+                    writer.write(outString);
+                    writer.newLine();
+                    outString = String.format("%d",item.getMinStock());
+                    writer.write(outString);
+                    writer.newLine();
+                    outString = String.format("%d",item.getMaxStock());
+                    writer.write(outString);
+                    writer.newLine();
+                    outString = String.format("%d",item.getRecommendedRetailPrice());
+                    writer.write(outString);
+                    writer.newLine();
+                    outString = String.format("%d",item.getTradeAllowance());
+                    writer.write(outString);
+                    writer.newLine();
+                    outString = String.format("%d",item.getCostPrice());
+                    writer.write(outString);
+                    writer.newLine();
+                    outString = String.format("%d",item.getSellingPrice());
+                    writer.write(outString);
+                    writer.newLine();
+                }
+                catch (Exception e) {
+
+                }
+            }
+            writer.flush();
+        }
+        catch (Exception e) {
+
+        }
+
+    }
 }
