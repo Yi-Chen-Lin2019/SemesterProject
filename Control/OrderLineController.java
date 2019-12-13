e332719c2527e312f2ca8606012e9da320c9ae2d
bandaniel2 <58685849+bandaniel2@users.noreply.github.com>
2019-12-13 08:57:49 +0100

    Update OrderLineController.java

--- Control/OrderLineController.java
+++ Control/OrderLineController.java
@@ -1,33 +1,131 @@
 package Control;
+
+
 import Model.*;
-import java.util.*;
+
+
 
 public class OrderLineController
 {
-    private static OrderLineController instance;
-    
-    private OrderLineController()
+
+    public OrderLineController()
     {
-        
     }
-    
-    public static OrderLineController getInstance()
+
+    public boolean addOrderLine(int orderId, String barcode, int quantity)
     {
-        if(instance == null)
+        OrderController oCtr = OrderController.getInstance();
+        GenericOrder order = oCtr.findOrder(orderId);
+        if (order == null)
         {
-            instance = new OrderLineController();
+            return false;
+            // This should not happen if we had chosen an existing order
         }
-        return instance;
+        
+        // Get the relevant itemdescriptor: Cooperation with the itemdescriptor controller
+        ItemController iCtr = new ItemController();
+        ItemDescriptor item =iCtr.getItem(barcode);
+        if (item == null)
+        {
+            return false;
+        }
+        // Create new suborder and link it to the found item descriptor
+        OrderLine ol= new OrderLine(quantity, item);
+        // Insert this suborder in the found order
+        return order.addOrderLine(ol);
     }
     
-    public OrderLine createOrderLine(int quantity,double price,String barcode)
+    public boolean addCopyOrderLine(int orderId, String barcode)
     {
-        OrderLine orderLine = new OrderLine(quantity, price, null); //need ItemController
-        return orderLine;
+        OrderController oCtr = new OrderController();
+        GenericOrder order = oCtr.findOrder(orderId);
+        if (order == null)
+        {
+            System.out.println("Error here");
+            return false;
+            // This should not happen if we had chosen an existing order
+        }
+        
+        // Get the relevant itemdescriptor: Cooperation with the itemdescriptor controller
+        ItemController iCtr = new ItemController();
+        Copy copy = iCtr.getCopy(barcode);
+        if (copy == null)
+        {
+            System.out.println("Error here2");
+            return false;
+        }
+        // Create new suborder and link it to the found item descriptor
+        OrderLineOfCopy oc= new OrderLineOfCopy(copy);
+        oc.setPrice(iCtr.getItem(barcode).getSellingPrice());
+        // Insert this suborder in the found order
+        return order.addCopyOrderLine(oc);
     }
-    public OrderLineOfCopy createOrderLineOfCopy(double price,String serialNumber)
+    
+    public boolean editOrderLine(int orderId, String barcode, int quantity)
     {
-        OrderLineOfCopy orderLine = new OrderLineOfCopy(price, null); //need ItemController
-        return orderLine;
+        OrderController oCtr = new OrderController();
+        GenericOrder order = oCtr.findOrder(orderId);
+        if (order == null)
+        {
+            return false;
+            // This should not happen if we had chosen an existing order
+        }
+  
+        // Get the relevant itemdescriptor: Cooperation with the itemdescriptor controller
+        ItemController iCtr = new ItemController();
+        ItemDescriptor item =iCtr.getItem(barcode);
+        if (item == null)
+        {
+            return false;
+        }
+        // Create new suborder and link it to the found item descriptor
+        OrderLine ol= new OrderLine(quantity, item);
+        // Insert this suborder in the found order
+        return order.addOrderLine(ol);
     }
+    
+    /*
+    public boolean removeOrderLine(int orderId, int orderLineNum)
+    {
+        OrderController oCtr = new OrderController();
+        GenericOrder order = oCtr.findOrder(orderId);
+        if (order == null)
+        {
+            System.out.println("Error");
+            return false;
+            // This should not happen if we had chosen an existing order
+        }
+        order = (CustomerOrder) order;
+        if(order.getOrderLine(orderLineNum)==null){
+            return false;
+        }
+        order.remove(order.getOrderLine(orderLineNum));
+        return true;
+    }
+    public boolean removeCopyOrderLine(OrderLineOfCopy orderLineOfCopy)
+    {
+    OrderController oCtr = new OrderController();
+        GenericOrder order = oCtr.findOrder(orderId);
+        if (order == null)
+        {
+            System.out.println("Error");
+            return false;
+            // This should not happen if we had chosen an existing order
+        }
+        
+        if(order.getOrderLine(orderLineNum)==null){
+            return false;
+        }
+        order.remove(order.getOrderLine(orderLineNum));
+        return true;
+    
+    }
+    */
+   
+    
+    
+    
+    
+    
+    
 }
